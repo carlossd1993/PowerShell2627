ee3cb2b569b95c4c65e1a7a34ef92d433f609899
carlossd1993 <carlos.sandom@educa.jcyl.es>
2026-09-23 22:58:16 +0200

    Ejercicio 1

---
+++ Ejercicios bucles/1.ps1
@@ -0,0 +1,5 @@
+<#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.#>
+$palabra = Read-Host "Introduce una palabra"
+for($i = 0; $i -lt 10; $i++){
+    Write-Host $palabra
+}
